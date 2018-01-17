d56e5c465b9cacdf26abc559dea6ad8cb5c88bb3
BCShirdavani <32500372+BCshirdavani@users.noreply.github.com>
2018-01-17 11:19:10 -0800

    Uploaded files
    To-Do:
     - adapt the collider so that it has the shape of a sculpting tool
     - change the cubes (smaller, neutral color)
     - Add hand tool object to sync with VR controllers

---
+++ DestroyCubes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCubes : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+//		if(col.gameObject.name.Substring(0,4) == "cube")
+//		{
+			Destroy(col.gameObject);
+			Debug.Log ("crash");
+//		}
+	}
+}
